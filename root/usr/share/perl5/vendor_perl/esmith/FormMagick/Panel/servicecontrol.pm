a932cf180ed0aba031d9f7015ee4eeb7092a0dbb
Stephane de Labrusse <stephdl@de-labrusse.fr>
2015-02-12 19:08:14 +0100

    Revert "work"
    This reverts commit 23197f8665c0e86136dbb06a10892c772b932f31.

--- root/usr/share/perl5/vendor_perl/esmith/FormMagick/Panel/servicecontrol.pm
+++ root/usr/share/perl5/vendor_perl/esmith/FormMagick/Panel/servicecontrol.pm
@@ -183,7 +183,6 @@ sub ServiceTCPChange {
         $service = 'httpd-e-smith' if ( $service eq 'modPerl');
         $service = 'qpsmtpd' if ( $service eq 'smtpd');
         $service = 'sqpsmtpd' if ( $service eq 'ssmtpd');
-        $service = 'spamd' if ( $service eq 'spamassassin');
 
 
    $startScript = glob("/etc/rc.d/rc7.d/S*$service");
@@ -243,8 +242,7 @@ sub ServiceChange {
         $service = 'httpd-e-smith' if ( $service eq 'modPerl');
         $service = 'qpsmtpd' if ( $service eq 'smtpd');
         $service = 'sqpsmtpd' if ( $service eq 'ssmtpd');
-        $service = 'spamd' if ( $service eq 'spamassassin');
-  
+            
     $startScript = glob("/etc/rc.d/rc7.d/S*$service");
     if ($startScript){     
     esmith::event::event_signal("service-one", $service, $action);}
@@ -567,7 +565,6 @@ sub AccessChange {
         $service = 'httpd-e-smith' if ( $service eq 'modPerl');
         $service = 'qpsmtpd' if ( $service eq 'smtpd');
         $service = 'sqpsmtpd' if ( $service eq 'ssmtpd');
-        $service = 'spamd' if ( $service eq 'spamassassin');
 
     $action="restart";
 
